753222f249e886e2acaf84e28fd8ff43a78be230
dgonzalez777 <1989.daniela@gmail.com>
2024-02-04 19:41:33 -0400

    Refactory

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,6 @@ function Board({ turn, squares, onPlay, isHistory }) {
     ];
     for (let ii = 0; ii < 8; ii++) {
       let [a, b, c] = winnerCases[ii];
-      // console.log(squares[a], squares[b], squares[c]);
       if (
         squares[a] &&
         squares[a] === squares[b] &&
@@ -87,20 +86,20 @@ function Board({ turn, squares, onPlay, isHistory }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  // const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext((prevState) => !prevState);
+    // setXIsNext((prevState) => !prevState);
   }
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
+    // setXIsNext(nextMove % 2 === 0);
   }
   const moves = history.map((squares, move) => {
     let description;
